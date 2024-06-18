29c21d80f9f2398c00f33a3f4de650e0c50be16b
TolikZ <tol1k.z4o@yandex.ru>
2024-06-18 12:37:47 +0300

    Update script_7.js
    update file

--- module10_homework/задание_7/script_7.js
+++ module10_homework/задание_7/script_7.js
@@ -1,39 +1,20 @@
 let myArray = [1, 2, 3, 4, 7, 100, 0, null, 0, "zero"];
+  let evenNum = 0;
+  let oddNum = 0;
+  let otherItem = 0;
 
-let oddNum = 0;
-let evenNum = 0;
-for (let i = 0; i < myArray.length; i++) {
-  if (myArray[i] % 2 == 0) {
-    evenNum++;
+  for (let i = 0; i < myArray.length; i++) {
+    if ((myArray[i] === 0) || (typeof (myArray[i]) != 'number')) {
+      otherItem++;
+    } else {
+      if (myArray[i] % 2 === 0) {
+        evenNum++;
+      } else {
+        oddNum++;
+      }
+    }
   }
-  else {
-    oddNum++;
-  }
-}
-console.log("Четные: " + evenNum);
-console.log("Нечетные: " + oddNum);
 
-let countNumber = 0;
-for (let i = 0; i < myArray.length; i++) {
-  if (typeof myArray[i] === "number") {
-    countNumber++;
-  }
-}
-console.log("Количество чисел в массиве: " + countNumber);
-
-let countString = 0;
-for (let i = 0; i < myArray.length; i++) {
-  if (typeof myArray[i] === "string") {
-    countString++;
-  }
-}
-console.log("Количество строк в массиве: " + countString);
-
-let itemZero = myArray.filter(value => value === 0).length;
-console.log("Нулей в массиве: " + itemZero);
-
-let itemNull = myArray.filter(value => value === null).length;
-console.log("Null в массиве: " + itemNull);
-
-let itemNan = myArray.filter(value => value === NaN).length;
-console.log("NaN в массиве: " + itemNan);
+  console.log("Четные: " + evenNum);
+  console.log("Нечетные: " + oddNum);
+  console.log("Другие: " + otherItem);
